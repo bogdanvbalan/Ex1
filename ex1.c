37ee454a834737e8827087af78f7301fd1173c91
Bogdan <bogdan.v.balan@gmail.com>
2018-11-07 04:48:08 -0800

    Added delete function.

--- ex1.c
+++ ex1.c
@@ -10,5 +10,20 @@ int main(){
 	add(11);
 	add(12);
 	print();
+	delete(2);
+	print();
+	delete(11);
+	print();
+	delete(12);
+	print();
+	delete(25);
+	print();
+	delete(0);
+	delete(-1);
+	add(10);
+	delete(7);
+	add(12);
+	add(15);
+	print();
 	return 0;
 }
